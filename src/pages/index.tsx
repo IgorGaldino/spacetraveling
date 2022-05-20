a97f846ba7c465c4b3e2ad44f4b63c0aa2c8f60e
Igor Galdino <igormgaldino@gmail.com>
2022-05-19 22:00:27 -0300

    Create post page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import { GetStaticProps } from 'next';
-import Link from "next/link";
+import Link from 'next/link';
 import { useState } from 'react';
-import { format } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR';
-
 import { FiCalendar, FiUser } from 'react-icons/fi';
 
+import { dateFormat } from '../utils/dateFormat';
 import { getPrismicClient } from '../services/prismic';
 import Header from '../components/Header';
 
@@ -35,7 +33,6 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
   const [posts, setPosts] = useState<PostPagination>(postsPagination);
 
   function onMorePost(): void {
-    console.log('Carregar mais posts', posts.next_page);
     fetch(posts.next_page)
       .then(response => response.json())
       .then(data => {
@@ -46,45 +43,45 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
       });
   }
 
-  function dateFormat(date): string {
-    return format(new Date(date), 'dd MMM yyyy', {
-      locale: ptBR,
-    });
-  }
-
   return (
-    <div className={commonStyles.container}>
+    <>
       <Header />
-      <section className={styles.posts}>
-        {posts.results.map(post => (
-          <article key={post.uid}>
-            <Link href={`/post/${post.uid}`}>
-              <a>
-                <h3>{post.data.title}</h3>
-              </a>
-            </Link>
-            <p>{post.data.subtitle}</p>
-            <div className={styles.info}>
-              <FiCalendar />
-              {dateFormat(post.first_publication_date)}
-              <FiUser />
-              {post.data.author}
-            </div>
-          </article>
-        ))}
-      </section>
-      {posts.next_page && (
-        <button
-          type="button"
-          className={styles.btnMorePosts}
-          onClick={() => {
-            onMorePost();
-          }}
-        >
-          Carregar mais posts
-        </button>
-      )}
-    </div>
+      <div className={commonStyles.container}>
+        <section className={styles.posts}>
+          {posts.results.map(post => (
+            <article key={post.uid}>
+              <Link href={`/post/${post.uid}`}>
+                <a>
+                  <h3>{post.data.title}</h3>
+                </a>
+              </Link>
+              <p>{post.data.subtitle}</p>
+              <div className={commonStyles.info}>
+                <span>
+                  <FiCalendar />
+                  {dateFormat(post.first_publication_date)}
+                </span>
+                <span>
+                  <FiUser />
+                  {post.data.author}
+                </span>
+              </div>
+            </article>
+          ))}
+        </section>
+        {posts.next_page && (
+          <button
+            type="button"
+            className={styles.btnMorePosts}
+            onClick={() => {
+              onMorePost();
+            }}
+          >
+            Carregar mais posts
+          </button>
+        )}
+      </div>
+    </>
   );
 }
 
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,8 +1,12 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { useEffect } from 'react';
 import { PrismicRichText } from '@prismicio/react';
-import Header from '../../components/Header';
+import { RichText } from 'prismic-dom';
 
+import Header from '../../components/Header';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+
+import { dateFormat } from '../../utils/dateFormat';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -29,28 +33,59 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post({ post }: PostProps, test): JSX.Element {
+export default function Post({ post }: PostProps): JSX.Element {
   useEffect(() => {
-    console.log('post', test);
-  }, [test]);
+    console.log('post', post);
+  }, [post]);
+  const WORD_PER_MIN = 200;
+  const readingTime = (): number => {
+    const wordsLength = post.data.content.reduce((acc, current) => {
+      const actualLenght = RichText.asText(current.body).match(/\S+/g).length;
+      return acc + actualLenght;
+    }, 0);
+    return Math.ceil(wordsLength / WORD_PER_MIN);
+  };
+
   return (
-    <div className={commonStyles.container}>
+    <>
       <Header />
-      <main>
-        <img src="" alt="" />
-        <section>
-          {post.data.content.map(content => {
-            console.log('content', content);
-            return (
-              <article key={content.heading}>
-                <h2>{content.heading}</h2>
-                <PrismicRichText field={content.body}  />
-              </article>
-            );
-          })}
-        </section>
-      </main>
-    </div>
+      {!!post?.data && (
+        <main>
+          <div className={styles.banner}>
+            <img src={post.data.banner.url} alt="banner" />
+          </div>
+          <section className={commonStyles.container}>
+            <header className={styles.header}>
+              <h2>{post.data.title}</h2>
+              <div className={commonStyles.info}>
+                <span>
+                  <FiCalendar />
+                  {dateFormat(post.first_publication_date)}
+                </span>
+                <span>
+                  <FiUser />
+                  {post.data.author}
+                </span>
+                <span>
+                  <FiClock />
+                  {readingTime()} min
+                </span>
+              </div>
+            </header>
+            <section className={styles.content}>
+              {post.data.content.map(content => {
+                return (
+                  <article key={content.heading}>
+                    <h2 className={styles.title}>{content.heading}</h2>
+                    <PrismicRichText field={content.body} />
+                  </article>
+                );
+              })}
+            </section>
+          </section>
+        </main>
+      )}
+    </>
   );
 }
 
